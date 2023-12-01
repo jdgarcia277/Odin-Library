bef29a6f39b0ed3d2fdf4cd72063ce03e42f62e2
jdgarcia277 <jdgarcia277@yahoo.com>
2023-12-01 16:12:23 -0600

    added links for catalog.html, also added homescreen card function for home & catalog

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-
+//event listener for home card link
+let cardLinkHome = document.querySelector('.card-links-home');
+cardLinkHome.addEventListener('click', function() {
+    window.location.href='index.html';
+})
+//event listener for catalog card link
+let cardLinkCatalog = document.querySelector('.card-links-catalog');
+cardLinkCatalog.addEventListener('click', function(){
+    window.location.href='catalog.html';
+})
   
