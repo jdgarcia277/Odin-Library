e7c169e38bf3c55ac0f524a596e1e5695bf520da
jdgarcia277 <jdgarcia277@yahoo.com>
2023-12-03 12:16:10 -0600

    repaired app.js from returning null for cardLinkHome and cardLinkCatalog

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 //event listener for home card link
 let cardLinkHome = document.querySelector('.card-links-home');
-cardLinkHome.addEventListener('click', function() {
-    window.location.href='index.html';
-})
+if(cardLinkHome !== null){
+    cardLinkHome.addEventListener('click', function() {
+        window.location.href='index.html';
+    })
+}
 //event listener for catalog card link
 let cardLinkCatalog = document.querySelector('.card-links-catalog');
-cardLinkCatalog.addEventListener('click', function(){
-    window.location.href='catalog.html';
-})
+if (cardLinkCatalog !== null){
+    cardLinkCatalog.addEventListener('click', function(){
+        window.location.href='catalog.html';
+    })
+}
+
   
